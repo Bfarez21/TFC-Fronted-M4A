c02daded9fdb238aa53a60aa97a30a54ea7d487c
Bfarez21 <bryanfareznieves@gmail.com>
2024-07-11 17:52:47 -0500

    Menu lateral desplegable (SideBar)

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
-export class SideBarComponent {
+export class SideBarComponent implements AfterViewInit {
 
     constructor(){}
+    ngAfterViewInit(): void {
+      const hamBurger = document.querySelector(".toggle-btn");
+  
+      if (hamBurger) {
+        hamBurger.addEventListener("click", function () {
+          const sidebar = document.querySelector("#sidebar");
+          if (sidebar) {
+            sidebar.classList.toggle("expand");
+          }
+        });
+      }
+    }
 }
+
+
