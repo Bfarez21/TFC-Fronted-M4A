267fc9cc80cc3da438e91f303fff3f5b15bd645a
Bfarez21 <bryanfareznieves@gmail.com>
2024-07-11 17:25:15 -0500

    creacion componente sideBar

---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+
+    constructor(){}
+}
