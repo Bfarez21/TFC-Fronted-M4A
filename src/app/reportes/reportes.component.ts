ed622b9f88daf71e66d0978e799fe08adf19734e
EdwinXMorocho <125777395+EdwinXMorocho@users.noreply.github.com>
2024-07-18 00:05:13 -0500

    reportes

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './reportes.component.css'
 })
 export class ReportesComponent {
+  patients = [
+    { nombre: 'Juan', cedula: '0106104971', razon: 'Gripe leve', edad: 22, carrera: 'Desarrollo de Software', fechaVisita: '21-03-2022', receta: 'Paracetamol al fallo' },
+    { nombre: 'Pedro', cedula: '0104597064', razon: 'Dolor de cabeza', edad: 31, carrera: 'Ingeniería Civil', fechaVisita: '10-07-2024', receta: 'Paracetamol' },
+  ];
 
+  viewPatient(patient: any) {
+    alert('Viewing patient: ' + patient.nombre);
+  }
+
+  editPatient(patient: any) {
+    alert('Editing patient: ' + patient.nombre);
+  }
+
+  deletePatient(patient: any) {
+    alert('Deleting patient: ' + patient.nombre);
+  }
 }
