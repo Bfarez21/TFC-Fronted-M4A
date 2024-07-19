395bfb18b65142ba8c461512a05e453139b66523
Bfarez21 <bryanfareznieves@gmail.com>
2024-07-18 23:12:09 -0500

    Correcion interfaz de ficha-medica

--- src/app/ficha-medica/ficha-medica.ts
+++ src/app/ficha-medica/ficha-medica.ts
@@ -1,7 +1,5 @@
 export class FichaMedica {
     id_fic:number=0;
-    antecedentes_medicos_fic:string[] = [];
-    discapacidad_fic:string="";
     fk_id_pac:number=0
     fecha_elaboracion_fic:string="";
 }
