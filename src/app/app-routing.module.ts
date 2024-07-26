30c8d7a181038bf648e553ff06b9a804a10ea26a
IsaacVilla11 <124637331+IsaacVilla11@users.noreply.github.com>
2024-07-25 19:54:23 -0500

    auth0 validado con @tecazuay.edu.ec

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
-import { AtencionMedicaComponent } from './atencion-medica/atencion-medica.component';
-import { FichaMedicaComponent } from './ficha-medica/ficha-medica.component';
-import { ReferenciaMedicaComponent } from './referencia-medica/referencia-medica.component';
-import { EnfermedadesComponent } from './enfermedades/enfermedades.component';
-import { DoctorComponent } from './doctor/doctor.component';
-import { ReportesComponent } from './reportes/reportes.component';
-import { SettingComponent } from './setting/setting.component';
-import { FormComponent } from './ficha-medica/form.component';
-import { FormRefMedicaComponent } from './referencia-medica/form-ref-medica/form-ref-medica.component';
-import { FormDocComponent } from './doctor/formdoc.component';
-import { FormAtencionMedicaComponent } from './atencion-medica/form-atencion-medica.component';
-import { InstitutoComponent } from './setting/instituto.component';
-import { FormEnfermedadesComponent } from './enfermedades/form.enfermedades.component';
-import { HomeComponent } from './home/home.component';
-import { AuthGuard } from '@auth0/auth0-angular';
 
-const routes: Routes = [
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  // { path: 'login', component: LoginComponent },
-  // { path: '**', redirectTo: '/home' }  // Ruta por defecto
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,16 +13,48 @@ export class AppComponent {
 
   isAuthenticated: boolean = false;
 
-  constructor(public auth: AuthService, private router: Router) {}
+  constructor(public auth: AuthService, private router: Router,
+    @Inject(DOCUMENT) private document: Document
+
+  ) { }
 
   ngOnInit() {
+    //Autenticación exitosa
     this.auth.isAuthenticated$.subscribe(isAuthenticated => {
       if (isAuthenticated) {
-        this.router.navigate(['/home']); 
+        this.router.navigate(['/home']);
       } else {
-        this.router.navigate(['/login']); 
+        this.router.navigate(['/login']);
       }
     });
+
+    //Errore de autenticación
+    this.auth.error$.subscribe(
+      (error) => {
+        console.log('Error recibido:', error);
+        if (error && error.message) {
+          Swal.fire({
+            title: 'Error',
+            text: error.message,
+            icon: 'error'
+          }).then(() => {
+            this.auth.logout({
+              logoutParams: {
+                returnTo: this.document.location.origin
+              }
+            }).subscribe(() => {
+              this.router.navigate(['/login']);
+            });
+          });
+        } else {
+          Swal.fire({
+            title: 'Error desconocido',
+            text: 'Ocurrió un error inesperado.',
+            icon: 'error'
+          });
+        }
+      }
+    );
   }
 
 }
