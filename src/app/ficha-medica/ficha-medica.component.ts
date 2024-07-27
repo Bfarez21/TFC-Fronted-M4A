fe44d14b98b9ca29a7fe85c8c5b96025939155a4
Bfarez21 <bryanfareznieves@gmail.com>
2024-07-27 16:10:46 -0500

    Ver actualizacion de ficha paciente

--- src/app/ficha-medica/ficha-medica.component.ts
+++ src/app/ficha-medica/ficha-medica.component.ts
@@ -11,12 +11,12 @@ import { AntecedenteFamiliarService } from './servicio/antecedente-familiar.serv
   templateUrl: './index-ficha-medica.component.html',
   styleUrl: './ficha-medica.component.css'
 })
-export class FichaMedicaComponent /*implements OnInit */{
-  fichaMedica:FichaMedica[] = []; // Asegúrate de inicializar pacientes como un arreglo vacío
+export class FichaMedicaComponent /*implements OnInit */ {
+  fichaMedica: FichaMedica[] = []; // Asegúrate de inicializar pacientes como un arreglo vacío
   cedulaBusqueda: string = '';
   fichaEncontrado: FichaMedica | null = null;
 
-  constructor(private fichaMedicaService: FichaMedicaService) {}
+  constructor(private fichaMedicaService: FichaMedicaService) { }
 
   ngOnInit(): void {
     this.cargarFichaMedica(); // Llama a cargarPacientes()
@@ -24,49 +24,61 @@ export class FichaMedicaComponent /*implements OnInit */{
 
   cargarFichaMedica(): void {
     this.fichaMedicaService.getFichasMedicas().subscribe(fichaMedica => {
-      fichaMedica = this.fichaMedica=fichaMedica; // Asigna los pacientes obtenidos del servicio a la propiedad del componente
+      fichaMedica = this.fichaMedica = fichaMedica; // Asigna los pacientes obtenidos del servicio a la propiedad del componente
     });
   }
 
-        /// BOTON VER
+  /// BOTON VER
   // uso Sweetalert para mostrar datos de un registro y asigno en el boton ver
-  verDetalle(fichaMedica:FichaMedica):void{
+  verDetalle(fichaMedica: FichaMedica): void {
     Swal.fire({
       title: `${fichaMedica.paciente.nombrePac} ${fichaMedica.paciente.apellidoPac}`,
       html: `
-        <p><strong>Cédula:</strong> ${fichaMedica.paciente.cedulaPac}</p>
-        <p><strong>Rol:</strong> ${fichaMedica.paciente.profesionPac}</p>
-        <p><strong>Fecha Nacimiento:</strong>${fichaMedica.paciente.fechaNacimientoPac} </p>
-        <p><strong>Lugar:</strong>${fichaMedica.paciente.lugarPac} </p>
-        <p><strong>Pais:</strong>${fichaMedica.paciente.paisPac} </p>
-        <p><strong>Dirección:</strong>${fichaMedica.paciente.direccionPac} </p>
-        <p><strong>Barrio:</strong>${fichaMedica.paciente.barrioPac} </p>
-        <p><strong>Parroquia:</strong>${fichaMedica.paciente.parroquiaPac} </p>
-        <p><strong>Cantón:</strong>${fichaMedica.paciente.cantonPac} </p>
-        <p><strong>Provincia:</strong>${fichaMedica.paciente.provinciaPac} </p>
-        <p><strong>Teléfono:</strong>${fichaMedica.paciente.telefonoPac} </p>
-        <p><strong>Género:</strong>${fichaMedica.paciente.generoPac} </p>
-        <p><strong>Estado Civil:</strong>${fichaMedica.paciente.estadoCivilPac} </p>
-        <p><strong>Tipo Sangre:</strong>${fichaMedica.paciente.tipoSangrePac} </p>
-        <p><strong>Carrera:</strong>${fichaMedica.paciente.carreraPac} </p>
-        <p><strong>Ciclo:</strong>${fichaMedica.paciente.cicloPac} </p>
-        <p><strong>Discapacidad:</strong>${fichaMedica.discapacidad.discapacidadG} </p>
-        <p><strong>Tipo:</strong>${fichaMedica.discapacidad.subtipoDis} </p>
-        <p><strong>Porcentaje:</strong>${fichaMedica.discapacidad.porcentajeDis} </p>
-        <p><strong>Carnet CONADIS:</strong>${fichaMedica.discapacidad.carnetCon} </p>    
-        <p><strong>Antecedentes:</strong>${fichaMedica.antecedenteFamiliar} </p>
-         <p><strong>Fecha Registro:</strong>${fichaMedica.fechaElaboracionFic} </p>
+      <div style="display: flex; flex-wrap: wrap;">
+        <div style="flex: 1; min-width: 200px; padding-right: 10px;">
+          <p><strong>Fecha Registro:</strong>${fichaMedica.fechaElaboracionFic} </p>
+          <p><strong>Cédula:</strong> ${fichaMedica.paciente.cedulaPac}</p>
+          <p><strong>Rol:</strong> ${fichaMedica.paciente.profesionPac}</p>
+          <p><strong>Fecha Nacimiento:</strong>${fichaMedica.paciente.fechaNacimientoPac} </p>
+          <p><strong>Lugar:</strong>${fichaMedica.paciente.lugarPac} </p>
+          <p><strong>Pais:</strong>${fichaMedica.paciente.paisPac} </p>
+          <p><strong>Dirección:</strong>${fichaMedica.paciente.direccionPac} </p>
+          <p><strong>Barrio:</strong>${fichaMedica.paciente.barrioPac} </p>
+          <p><strong>Parroquia:</strong>${fichaMedica.paciente.parroquiaPac} </p>
+          <p><strong>Cantón:</strong>${fichaMedica.paciente.cantonPac} </p>
+          <p><strong>Provincia:</strong>${fichaMedica.paciente.provinciaPac} </p>
+          <p><strong>Teléfono:</strong>${fichaMedica.paciente.telefonoPac} </p>
+        </div>
+        <div style="flex: 1; min-width: 200px;">
+          <p><strong>Género:</strong>${fichaMedica.paciente.generoPac} </p>
+          <p><strong>Estado Civil:</strong>${fichaMedica.paciente.estadoCivilPac} </p>
+          <p><strong>Tipo Sangre:</strong>${fichaMedica.paciente.tipoSangrePac} </p>
+          <p><strong>Carrera:</strong>${fichaMedica.paciente.carreraPac} </p>
+          <p><strong>Ciclo:</strong>${fichaMedica.paciente.cicloPac} </p>
+          <p><strong>Discapacidad:</strong>${fichaMedica.discapacidad.discapacidadG} </p>
+          <p><strong>Tipo:</strong>${fichaMedica.discapacidad.subtipoDis} </p>
+          <p><strong>Porcentaje:</strong>${fichaMedica.discapacidad.porcentajeDis} </p>
+          <p><strong>Carnet CONADIS:</strong>${fichaMedica.discapacidad.carnetCon} </p>    
+          <p><strong>Antecedentes:</strong> </p>
+          <p><strong>Alérgia:</strong>${fichaMedica.antecedenteFamiliar.alergiaAnt} </p>
+          <p><strong>Clínico:</strong>${fichaMedica.antecedenteFamiliar.clinicoAnt} </p>
+          <p><strong>Ginecológico:</strong>${fichaMedica.antecedenteFamiliar.ginecologoAnt} </p>
+          <p><strong>Traumatológico:</strong>${fichaMedica.antecedenteFamiliar.traumatologicoAnt} </p>
+          <p><strong>Quirúrgico:</strong>${fichaMedica.antecedenteFamiliar.quirurgicoAnt} </p>
+          <p><strong>Farmacológico:</strong>${fichaMedica.antecedenteFamiliar.farmacologicoAnt} </p>
+        </div>
+      </div>  
       `,
     });
   }
   //BOTON BUSCAR
-  buscar():void{
+  buscar(): void {
 
   }
   /// BOTON ELIMINAR
   //eliminar datos de la base
-  deleteFicha():void{
-    
+  deleteFicha(): void {
+
   }
 
 }
